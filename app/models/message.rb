fa59d0577dcb3447dd982438c64b172dd6fc758c
yurika01 <yurikapo01@yahoo.co.jp>
2015-08-23 14:16:03 +0000

    repair2

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,7 +2,7 @@ class Message < ActiveRecord::Base
     # 名前は必須鵜入力かつ20文字以内
     validates :name , length: { maximum: 20 } , presence: true
      # 年齢は必須鵜入力かつ3文字以内
-    validates :age , length: { maximum: 3 } , presence: true
+    validates :age , length: { maximum: 3 }, numericality: { only_integer: true, greater_than_or_wqual_to: 0 }, presence: true
     #内容は必須入力かつ2文字以上30文字以内
     validates :body , length: {minimum: 2 , maximum: 30 } , presence: true
 end
